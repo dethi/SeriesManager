d87e377ee3cbc124375c208c9868a9e25faf9623
Thibault Deutsch <thibault.deutsch@gmail.com>
2012-06-25 17:05:54 +0200

    Add information to be displayed.

--- SeriesManager.py
+++ SeriesManager.py
@@ -22,10 +22,12 @@ _CWD_ORIGINE = os.getcwd()
 #-----------------------------------------------------------------------------
 
 def main():
+    print("+-----------------------------+")
     file_pref = os.getcwd() + "/pref.cfg"
     if not os.path.isfile(file_pref):
         first_start()
     get_opts()
+    print("+-----------------------------+")
         
 def first_start():
     pref = dict()
@@ -57,12 +59,9 @@ def get_opts():
             if os.path.isdir(a):
                 auto_detect(a, cfg)
             else:
-                print("This is not a valid folder. ", end="")
+                print("|| This is not a valid folder. ", end="")
                 print("Please see the documentation.")
                 usage()
-        else:
-            print("Option {} inconnue".format(o))
-            sys.exit(2)
 
 def usage():
     pass
@@ -71,6 +70,8 @@ def read_config():
     pass
 
 def auto_detect(dir, cfg):
+    print("| Starts detection...")
+    
     dir.replace("\\", "/")
     if re.search(r"/{1}$", dir):
         dir = dir[:len(dir)-1]
@@ -88,21 +89,21 @@ def auto_detect(dir, cfg):
         dir_cwd = "/".join(dir_cwd) + "/"
         os.chdir(dir_cwd)
     
-    if os.path.isdir(dir):
-        detect_season(dir)
-    else:
-        print("This is not a valid folder.")
+    detect_season(dir)
 
 def detect_season(folder):
     re_season = re.compile(r"(s|(s[aie]{2}sons?))[ .-]?[0-9]+")
     re_movie_file = re.compile(r"[(.avi)(.mkv)(.flv)(.mp4)(.m4v)(.wmv)]$")
     
     if re_season.search(folder.lower()):
+        print("| Folder season detected.")
         folder_list = list()
         folder_list.append(folder)
+        print("| Renames folder season...")
         folder_list = rename_season(folder_list)
         detect_episode(folder_list[0])
     else:
+        print("| Integral series detected.")
         os.chdir(folder)
         folder_list = os.listdir()
         other_file = list()
@@ -111,32 +112,44 @@ def detect_season(folder):
                 if re_movie_file.search(elt.lower()):
                     other_file.append(elt)
                 else:
+                    print("| Removes other files (like .nfo)...")
                     os.remove(elt)
+                    print("|---> {}".format(elt))
                     del folder_list[i]
+        print("| Renames folders seasons...")
         folder_list = rename_season(folder_list)
+        print("| Starts detection of episodes...")
         for elf in folder_list:
             detect_episode(elf)
 
 def detect_episode(season_folder):
+    print("| Process began in folder \"{}\"".format(season_folder))
     re_movie_file = re.compile(r"[(.avi)(.mkv)(.flv)(.mp4)(.m4v)(.wmv)]$")
     os.chdir(season_folder)
     file_list = os.listdir()
     episode_list = list()
     for i,elt in enumerate(file_list):
         if os.path.isdir(elt):
+            print("| Episode is in separate folders.")
             os.chdir(elt)
             tmp_file_list = os.listdir()
+            print("| Moving episodes in a single folder.")
             for file in tmp_file_list:
                 if re_movie_file.search(file):
                     shutil.move(file, "..")
                     episode_list.append(file)
             os.chdir("..")
+            print("| Delete the old folder.")
             shutil.rmtree(elt)
         else:
             if re_movie_file.search(elt.lower()):
                 episode_list.append(elt)
             else:
+                print("| Removes other files (like .nfo)...")
+                print("|---> {}".format(elt))
                 os.remove(elt)
+    
+    print("| Renames episodes...")
     rename_episode(episode_list)
     os.chdir("..")
            
@@ -149,6 +162,7 @@ def rename_season(folder_list):
         elt = result.group("id")
         elt = "Season " + elt
         new_folder_list.append(elt)
+        print("|---> {} ==> {}".format(temp_name, elt))
         os.rename(temp_name, elt)
     return new_folder_list
 
@@ -162,11 +176,12 @@ def rename_episode(episode_list):
             file_extention = "." + file_extention[len(file_extention)-1]
             elt = result.group("id")
             elt = elt + file_extention
+            print("|---> {} ==> {}".format(temp_name, elt))
             os.rename(temp_name, elt)
         else:
-            print("Episode number undetected. ", end="")
+            print("|| Episode number undetected. ", end="")
             print("Please rename the episode manually.")
-            print(str(os.getcwd() + "/" + elt))
+            print("||-->>" + str(os.getcwd() + "/" + elt))
 
 if __name__ == "__main__":
     main()
