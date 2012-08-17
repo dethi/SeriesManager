60307355047637594091c406f5735ab0dad9b172
Thibault Deutsch <thibault.deutsch@gmail.com>
2012-08-17 03:40:35 +0200

    Add color, verbose mode, new syntax and fixes bugs.

--- SeriesManager.py
+++ SeriesManager.py
@@ -7,10 +7,11 @@ import shutil
 import getopt
 import re
 
+
 # Globals variables
 #-----------------------------------------------------------------------------
 
-_VERSION = "0.0.3"
+_VERSION = "0.0.4"
 _AUTHOR = "Deutsch Thibault"
 _EMAIL = "thibault.deutsch@gmail.com"
 _WEB = "http://www.thionnux.fr/"
@@ -18,63 +19,114 @@ _WEB = "http://www.thionnux.fr/"
 _CWD_ORIGINE = os.getcwd()
 #CFG = dict()
 
+VERBOSE = False
+
+# Classes
+#-----------------------------------------------------------------------------
+
+class colors:
+    """Defined colors"""
+    
+    RED = '\033[91m'
+    GREEN = '\033[92m'
+    NO = '\033[0m'
+    
+class disp:
+    """Personnal print-like fonction."""
+    
+    def verbose(function):
+        """Manages the verbose mode"""
+        def verbose_verification(*args, **kwargs):
+            global VERBOSE
+            if VERBOSE:
+                return function(*args, **kwargs)
+        return verbose_verification
+    
+    def error(*args, **kwargs):
+        """Print error."""
+        print("|" + colors.RED + "|| ", end="")
+        print(*args, **kwargs)
+        print(colors.NO, end="")
+        
+    @verbose
+    def good(*args, **kwargs):
+        """Print validation."""
+        print("|" + colors.GREEN + "---> ", end="")
+        print(*args, **kwargs)
+        print(colors.NO, end="")
+        
+    def info(*args, **kwargs):
+        """Print info."""
+        print("|", *args, **kwargs)
+        
+    def line():
+        """Print a long line."""
+        print("+" + "-" * 70 + "+")
+
+
 # Functions
 #-----------------------------------------------------------------------------
 
 def main():
     """Start the script."""
-    print("+--------------------------------------------------+")
-    file_pref = os.getcwd() + "/pref.cfg"
+    disp.line()
+    file_pref = os.getcwd() + "/pref.ini"
     if not os.path.isfile(file_pref):
         first_start()
     get_opts()
-    print("+--------------------------------------------------+")
+    disp.line()
         
 def first_start():
     """Create config file at first start."""
     pref = dict()
-    with open("pref.cfg", "w") as file:
+    with open("pref.ini", "w") as file:
         pref["naming"] = input("Naming : ")
         
         pref_str = str(pref).replace(", ", "\n").replace(": ", "=")
         pref_str = pref_str.replace("'", "")
         file.write(pref_str[1:len(pref_str)-1])
+        
+    syntax()
+    
 
 def get_opts():
     """Addresses the arguments passed to the command line."""
-    cfg = read_config()
     
     try:
-        opts, args = getopt.getopt(sys.argv[1:], "vha:", ["help", "add="])
+        opts, args = getopt.getopt(sys.argv[1:], "vh", ["help"])
     except getopt.GetoptError as err:
-        print(err)
-        usage()
+        disp.error(err)
+        disp.line()
         sys.exit(2)
     
     for o, a in opts:
         if o == "-v":
-            #global VERBOSE
-            #VERBOSE = True
-            pass
+            global VERBOSE = True
         elif o in ("-h", "-help"):
-            usage()
-        elif o in ("-a", "--add"):
-            if os.path.isdir(a):
-                auto_detect(a, cfg)
-            else:
-                print("||| This is not a valid folder. ", end="")
-                print("Please see the documentation.")
-                usage()
+            syntax()
+                
+    if len(args) == 1:
+        if os.path.isdir(args[0]):
+            auto_detect(args[0])
+        else:
+            disp.error("This is not a valid folder.", 
+                "Please see the documentation.")
+            syntax()
+    else:
+        disp.error("You have not specified folder.",
+            "Please see the documentation.")
+        syntax()
 
-def usage():
-    pass
+def syntax():
+    disp.line()
+    sys.exit(0)
 
 def read_config():
     pass
 
-def auto_detect(dir, cfg):
+def auto_detect(dir):
     """Analyzes the folder supplied as an argument."""
-    print("| Starts detection...")
+    disp.info("Starts detection...")
     
     dir.replace("\\", "/")
     if re.search(r"/{1}$", dir):
@@ -97,18 +149,18 @@ def auto_detect(dir, cfg):
 
 def detect_season(folder):
     """Detects seasons."""
-    re_season = re.compile(r"(s|(s[aie]{2}sons?))[ .-]?[0-9]+")
+    re_season = re.compile(r"(s|(s[aie]{2}sons?))[ .]?[0-9]+")
     re_movie_file = re.compile(r"[(.avi)(.mkv)(.flv)(.mp4)(.m4v)(.wmv)]$")
     
     if re_season.search(folder.lower()):
-        print("| Folder season detected.")
+        disp.info("Folder season detected.")
         folder_list = list()
         folder_list.append(folder)
-        print("| Renames folder season...")
+        disp.info("Renames folder season...")
         folder_list = rename_season(folder_list)
         detect_episode(folder_list[0])
     else:
-        print("| Integral series detected.")
+        disp.info("Integral series detected.")
         os.chdir(folder)
         folder_list = os.listdir()
         other_file = list()
@@ -117,66 +169,66 @@ def detect_season(folder):
                 if re_movie_file.search(elt.lower()):
                     other_file.append(elt)
                 else:
-                    print("| Removes other files (like .nfo)...")
+                    disp.info("Removes other files (like .nfo)...")
                     os.remove(elt)
-                    print("|---> {}".format(elt))
+                    disp.good("{}".format(elt))
                     del folder_list[i]
-        print("| Renames folders seasons...")
+        disp.info("Renames folders seasons...")
         folder_list = rename_season(folder_list)
-        print("| Starts detection of episodes...")
+        disp.info("Starts detection of episodes...")
         for elf in folder_list:
             detect_episode(elf)
 
 def detect_episode(season_folder):
     """Detects episodes."""
-    print("| Process began in folder \"{}\"".format(season_folder))
+    disp.info("Process began in folder \"{}\"".format(season_folder))
     re_movie_file = re.compile(r"[(.avi)(.mkv)(.flv)(.mp4)(.m4v)(.wmv)]$")
     os.chdir(season_folder)
     file_list = os.listdir()
     episode_list = list()
     for i,elt in enumerate(file_list):
         if os.path.isdir(elt):
-            print("| Episode is in separate folders.")
+            disp.info("Episode is in separate folders.")
             os.chdir(elt)
             tmp_file_list = os.listdir()
-            print("| Moving episodes in a single folder.")
+            disp.info("Moving episodes in a single folder.")
             for file in tmp_file_list:
                 if re_movie_file.search(file):
                     shutil.move(file, "..")
                     episode_list.append(file)
             os.chdir("..")
-            print("| Delete the old folder.")
+            disp.info("Delete the old folder.")
             shutil.rmtree(elt)
         else:
             if re_movie_file.search(elt.lower()):
                 episode_list.append(elt)
             else:
-                print("| Removes other files (like .nfo)...")
-                print("|---> {}".format(elt))
+                disp.info("Removes other files (like .nfo)...")
+                disp.good("{}".format(elt))
                 os.remove(elt)
     
-    print("| Renames episodes...")
+    disp.info("Renames episodes...")
     rename_episode(episode_list)
     os.chdir("..")
            
 def rename_season(folder_list):
     """Renames seasons."""
     new_folder_list = list()
-    re_season = re.compile(r"(s|(s[aie]{2}sons?))[ .-]?(?P<id>[0-9]{1,2})")
+    re_season = re.compile(r"(s|(s[aie]{2}sons?))[ .]?(?P<id>[0-9]{1,2})")
     for elt in folder_list:
         temp_name = elt
         result = re_season.search(elt.lower())
         elt = result.group("id")
         elt = int(elt)
-        elt = "Season {}".format(elt)
+        elt = "Saison {}".format(elt)
         new_folder_list.append(elt)
-        print("|---> {} ==> {}".format(temp_name, elt))
+        disp.good("{} ==> {}".format(temp_name, elt))
         os.rename(temp_name, elt)
     return new_folder_list
 
 def rename_episode(episode_list):
     """Renames episodes."""
-    re_episode = re.compile(r"(e|(episode))[.-]?(?P<id>[0-9]{1,2})")
+    re_episode = re.compile(r"(e|(ep(isode)?))(?P<id>[0-9]{1,2})")
     new_name = list()
     for elt in episode_list:
         temp_name = elt
@@ -193,16 +245,16 @@ def rename_episode(episode_list):
             if not elt in new_name:
                 os.rename(temp_name, elt)
                 new_name.append(elt)
-                print("|---> {} ==> {}".format(temp_name, elt))
+                disp.good("{} ==> {}".format(temp_name, elt))
             else:
-                print("||| Episode number \"{}\" already exists.".format(elt),
-                    end="")
-                print(" Please rename the episode manually.")
-                print("|||-->> {}".format(temp_name))
+                disp.error("Episode number \"{}\" already exists.".format(elt),
+                    "Please rename the episode manually.")
+                disp.error("-->> {}".format(temp_name))
         else:
-            print("||| Episode number undetected. ", end="")
-            print("Please rename the episode manually.")
-            print("|||-->>" + str(os.getcwd() + "/" + elt))
+            disp.error("Episode number undetected.", 
+                "Please rename the episode manually.")
+            disp.error("-->> " + str(os.getcwd() + "/" + elt))
+
 
 if __name__ == "__main__":
     main()
